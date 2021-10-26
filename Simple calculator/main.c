1764b75e7e77c70ac4877e8754a7695dd345f568
07japhet <90698701+07japhet@users.noreply.github.com>
2021-10-26 16:34:01 +0300

    Update main.c

--- Simple calculator/main.c
+++ Simple calculator/main.c
@@ -1,3 +1,10 @@
+Project title:simple caclutor
+Author:Japhet Makori
+Compiler:GCC
+License:MIT
+
+
+
 /*create  a calculator program that will accept two numbers and compute the sum difference, product, quotient and modulus of the 2 numbers
 
  INPUTS:
